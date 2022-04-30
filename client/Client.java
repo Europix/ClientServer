7a7838acbff1a6eb98937aa066a72eca4b794435
6 43 <mercury1244@outlook.com>
2022-04-30 13:23:09 +0800

    cleaned up code

--- client/Client.java
+++ client/Client.java
@@ -3,129 +3,126 @@ import java.net.Socket;
 
 
 public class Client {
-	private Socket client = null;
-	public void starClient(String[] args) {
+    private Socket client = null;
 
-		try {
-			client = new Socket("localhost",5000);
-			{
-				String str = String.valueOf(args[0]);
-				boolean b = !str.equals("totals") && !str.equals("list") && !str.equals("join");
-				if (!b) {
-					PrintStream ps = new PrintStream(client.getOutputStream());
-					if(str.equals("totals")){  // Valid arguments
-						if(args.length == 1) {
-							ps.println(str);
-							InputStream is = client.getInputStream();
-							BufferedReader br = new BufferedReader(new InputStreamReader(is));
-							String line;
-							line = br.readLine();
-							System.out.println(line);
-							String[] elem = line.split(" ");
-							for (int i = 1; i <= Integer.parseInt(elem[2]); i++) {
-								line = br.readLine();
-								System.out.println(line);
-							}
-						}
-						else {
-							System.out.println("Invalid Arguments For 'Total' !\n");
-							client.close();
-						}
-					}
-					if(str.equals("list")){
-						if(args.length == 2) { // Valid arguments
-							boolean flag = true;
-							try {
-								Integer.parseInt(args[1]);
-							} catch (Exception e) {
-								System.out.println("Invalid Arguments For list Number!\n");
-								flag = false;
-								client.close();
-							}
-							if (flag) {
-								ps.println(str);
-								for (int i=1; i< args.length; i++) {
-									String str1 = String.valueOf(args[i]);
-									ps.println(str1);
-								}
-								InputStream is = client.getInputStream();
-								BufferedReader br = new BufferedReader(new InputStreamReader(is));
-								String line = "qwq";
-								while(!line.equals("It_is_the_end")){ // catches the mark of end
-									try {
-										line = br.readLine();
-										if(!line.equals("It_is_the_end"))System.out.println(line);
-										if(line.equals("Invalid Parameter!"))client.close();
-									}
-									catch (Exception e){ // Socket Exception
-										client.close();
-										break;
-									}
-								}
-								client.close(); // Ensure the client is closed.
-							}
-						}
-						else {
-							System.out.println("Invalid Arguments For 'list' !\n");
-							client.close();
-						}
-					}
-					if(str.equals("join")){
-						if(args.length == 3) { // Join Requires 3 arguments.
-							InputStream is = client.getInputStream();
-							boolean flag = true;
-							try {
-								Integer.parseInt(args[1]);
-							} catch (Exception e) {
-								System.out.println("Invalid Arguments for List Number!\n");
-								flag = false;
-								client.close();
-							}
-							if(flag) {
-								ps.println(str);
-								for (int i = 1; i < args.length; i++) {
-									String str1 = String.valueOf(args[i]);
-									ps.println(str1);
-								}
-								try {
-									BufferedReader br = new BufferedReader(new InputStreamReader(is));
-									String line;
-									line = br.readLine();
-									System.out.println(line);
-								} catch (Exception e) {
-									e.printStackTrace();
-								}
-							}
-						}
-						else {
-							System.out.println("Invalid Arguments For 'Join' !\n");
-							client.close();
-						}
-					}
-				}
-				else {
-					System.out.println("Wrong Arguments!\nClient will close.");
-					client.close();
-				}
+    public static void main(String[] args) {
+        Client test = new Client();
+        test.starClient(args);
+    }
 
-			}
+    public void starClient(String[] args) {
+
+        try {
+            client = new Socket("localhost", 5000);
+            {
+                String str = String.valueOf(args[0]);
+                boolean b = !str.equals("totals") && !str.equals("list") && !str.equals("join");
+                if (!b) {
+                    PrintStream ps = new PrintStream(client.getOutputStream());
+                    if (str.equals("totals")) {  // Valid arguments
+                        if (args.length == 1) {
+                            ps.println(str);
+                            InputStream is = client.getInputStream();
+                            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                            String line;
+                            line = br.readLine();
+                            System.out.println(line);
+                            String[] elem = line.split(" ");
+                            for (int i = 1; i <= Integer.parseInt(elem[2]); i++) {
+                                line = br.readLine();
+                                System.out.println(line);
+                            }
+                        } else {
+                            System.out.println("Invalid Arguments For 'Total' !\n");
+                            client.close();
+                        }
+                    }
+                    if (str.equals("list")) {
+                        if (args.length == 2) { // Valid arguments
+                            boolean flag = true;
+                            try {
+                                Integer.parseInt(args[1]);
+                            } catch (Exception e) {
+                                System.out.println("Invalid Arguments For list Number!\n");
+                                flag = false;
+                                client.close();
+                            }
+                            if (flag) {
+                                ps.println(str);
+                                for (int i = 1; i < args.length; i++) {
+                                    String str1 = String.valueOf(args[i]);
+                                    ps.println(str1);
+                                }
+                                InputStream is = client.getInputStream();
+                                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                                String line = "qwq";
+                                while (!line.equals("It_is_the_end")) { // catches the mark of end
+                                    try {
+                                        line = br.readLine();
+                                        if (!line.equals("It_is_the_end")) System.out.println(line);
+                                        if (line.equals("Invalid Parameter!")) client.close();
+                                    } catch (Exception e) { // Socket Exception
+                                        client.close();
+                                        break;
+                                    }
+                                }
+                                client.close(); // Ensure the client is closed.
+                            }
+                        } else {
+                            System.out.println("Invalid Arguments For 'list' !\n");
+                            client.close();
+                        }
+                    }
+                    if (str.equals("join")) {
+                        if (args.length == 3) { // Join Requires 3 arguments.
+                            InputStream is = client.getInputStream();
+                            boolean flag = true;
+                            try {
+                                Integer.parseInt(args[1]);
+                            } catch (Exception e) {
+                                System.out.println("Invalid Arguments for List Number!\n");
+                                flag = false;
+                                client.close();
+                            }
+                            if (flag) {
+                                ps.println(str);
+                                for (int i = 1; i < args.length; i++) {
+                                    String str1 = String.valueOf(args[i]);
+                                    ps.println(str1);
+                                }
+                                try {
+                                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                                    String line;
+                                    line = br.readLine();
+                                    System.out.println(line);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        } else {
+                            System.out.println("Invalid Arguments For 'Join' !\n");
+                            client.close();
+                        }
+                    }
+                } else {
+                    System.out.println("Wrong Arguments!\nClient will close.");
+                    client.close();
+                }
+
+            }
 
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (client != null) {
-				try {
-					client.close();
-				} catch (IOException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (IOException e) {
 // TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	public static void main(String[] args) {
-		Client test = new Client();
-		test.starClient(args);
-	}
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- server/Server.java
+++ server/Server.java
@@ -1,128 +1,127 @@
-
 import java.io.*;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Server {
-	public void starServer(String[] args){
-		int List_Number;
-		int Max_Member;
-		if (Integer.parseInt(args[0]) <= 0 || Integer.parseInt(args[1]) <= 0){
-			System.out.println("Error: Invalid Arguments, Server will close.");
-			return;
-		}
-		List_Number = Integer.parseInt(args[0]); // Max lists
-		Max_Member = Integer.parseInt(args[1]); // Max members
-		ServerSocket server = null;
-		String[][] list = new String[List_Number+1][Max_Member+1];
+    public static void main(String[] args) {
+        Server test = new Server();
+        test.starServer(args);
+    }
 
-		try {
-			server = new ServerSocket(5000); // New Server Started
-			//System.out.println("server started successfully");
-		} catch (IOException e1) {
+    public void starServer(String[] args) {
+        int List_Number;
+        int Max_Member;
+        if (Integer.parseInt(args[0]) <= 0 || Integer.parseInt(args[1]) <= 0) {
+            System.out.println("Error: Invalid Arguments, Server will close.");
+            return;
+        }
+        List_Number = Integer.parseInt(args[0]); // Max lists
+        Max_Member = Integer.parseInt(args[1]); // Max members
+        ServerSocket server = null;
+        String[][] list = new String[List_Number + 1][Max_Member + 1];
+
+        try {
+            server = new ServerSocket(5000); // New Server Started
+            //System.out.println("server started successfully");
+        } catch (IOException e1) {
 // TODO Auto-generated catch block
-			e1.printStackTrace();
-			//System.out.println("Error: catch an Exception, Failed to initialize");
-		}
+            e1.printStackTrace();
+            //System.out.println("Error: catch an Exception, Failed to initialize");
+        }
 
-		Socket client;
-		while(true) {
-			try {
-				assert server != null;
-				client = server.accept();
-				InputStream is = client.getInputStream();
-				BufferedReader br = new BufferedReader(new InputStreamReader(is));
-				String line = br.readLine();
-				int count;
-				File file =new File("log.txt");
-				//if file doesnt exists, then create it
-				if(!file.exists()){
-					boolean success = file.createNewFile();
-				}
-				if (line != null) {
-					FileWriter fw = new FileWriter("log.txt",true);
-					BufferedWriter bw = new BufferedWriter(fw);
-					SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd '|' HH:mm:ss ");
-					Date date = new Date(System.currentTimeMillis());
-					String date_data = formatter.format(date);
-					String IP = client.getInetAddress().toString().substring(1); // Delete the prefix "/"
-					String log = date_data + '|' + IP + '|' + ' ' + line + '\n';
-					bw.write(log);
-					bw.close();
-					if (line.equals("totals")) {
-						PrintStream ps = new PrintStream(client.getOutputStream());
-						ps.printf("There are %d list(s), each with a maximum size of %d.\n", List_Number, Max_Member);
-						try {
-							for (int i = 0; i < List_Number; i++) {
-								count = 0;
-								for (int j = 0; j < Max_Member; j++)
-									if (list[i][j] != null && list[i][j].length() != 0) count++;
-								ps.printf("List %d has %d member(s).\n", i + 1, count);
-							}
-							ps.print("\n");
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
+        Socket client;
+        while (true) {
+            try {
+                assert server != null;
+                client = server.accept();
+                InputStream is = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String line = br.readLine();
+                int count;
+                File file = new File("log.txt");
+                //if file doesnt exists, then create it
+                if (!file.exists()) {
+                    boolean success = file.createNewFile();
+                }
+                if (line != null) {
+                    FileWriter fw = new FileWriter("log.txt", true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd '|' HH:mm:ss ");
+                    Date date = new Date(System.currentTimeMillis());
+                    String date_data = formatter.format(date);
+                    String IP = client.getInetAddress().toString().substring(1); // Delete the prefix "/"
+                    String log = date_data + '|' + IP + '|' + ' ' + line + '\n';
+                    bw.write(log);
+                    bw.close();
+                    if (line.equals("totals")) {
+                        PrintStream ps = new PrintStream(client.getOutputStream());
+                        ps.printf("There are %d list(s), each with a maximum size of %d.\n", List_Number, Max_Member);
+                        try {
+                            for (int i = 0; i < List_Number; i++) {
+                                count = 0;
+                                for (int j = 0; j < Max_Member; j++)
+                                    if (list[i][j] != null && list[i][j].length() != 0) count++;
+                                ps.printf("List %d has %d member(s).\n", i + 1, count);
+                            }
+                            ps.print("\n");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
 
-					}
-					if (line.equals("list")) {
-						String para = br.readLine();
-						PrintStream ps = new PrintStream(client.getOutputStream());
-						int index = Integer.parseInt(para) - 1 ; // begin with 1
-						if(index < 0 || index >= List_Number){
-							ps.println("Invalid Parameter!");
-							continue;
-						}
-						for (int i = 0; i < Max_Member; i++) {
-							if (list[index][i] != null && list[index][i].length() != 0) {
-								ps.println(list[index][i]);
-							}
-						}
-						ps.println("It_is_the_end"); // End mark.
-					}
-					if (line.equals("join")) {
-						try {
-							PrintStream ps = new PrintStream(client.getOutputStream());
-							String para = br.readLine();
-							String name = br.readLine();
-							//System.out.println(name);
-							int index = Integer.parseInt(para) - 1 ; // Begin From 1
-							if(index < 0 || index >= List_Number){
-								ps.println("Invalid Parameter!");
-								continue;
-							}
-							int cnt = 0;
-							for (int i = 0; i < Max_Member; i++) {
-								if (list[index][i] != null && list[index][i].length() != 0)
-									cnt++;
-							}
-							if (cnt < Max_Member) {
-								// name = name.replace("\"", "");
-								list[index][cnt] = name;
-								//System.out.println(cnt);
-								ps.println("Success!");
-							} else {
-								//System.out.println("qwqwq");
-								ps.println("Failed!");
-							}
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				}
+                    }
+                    if (line.equals("list")) {
+                        String para = br.readLine();
+                        PrintStream ps = new PrintStream(client.getOutputStream());
+                        int index = Integer.parseInt(para) - 1; // begin with 1
+                        if (index < 0 || index >= List_Number) {
+                            ps.println("Invalid Parameter!");
+                            continue;
+                        }
+                        for (int i = 0; i < Max_Member; i++) {
+                            if (list[index][i] != null && list[index][i].length() != 0) {
+                                ps.println(list[index][i]);
+                            }
+                        }
+                        ps.println("It_is_the_end"); // End mark.
+                    }
+                    if (line.equals("join")) {
+                        try {
+                            PrintStream ps = new PrintStream(client.getOutputStream());
+                            String para = br.readLine();
+                            String name = br.readLine();
+                            //System.out.println(name);
+                            int index = Integer.parseInt(para) - 1; // Begin From 1
+                            if (index < 0 || index >= List_Number) {
+                                ps.println("Invalid Parameter!");
+                                continue;
+                            }
+                            int cnt = 0;
+                            for (int i = 0; i < Max_Member; i++) {
+                                if (list[index][i] != null && list[index][i].length() != 0)
+                                    cnt++;
+                            }
+                            if (cnt < Max_Member) {
+                                // name = name.replace("\"", "");
+                                list[index][cnt] = name;
+                                //System.out.println(cnt);
+                                ps.println("Success!");
+                            } else {
+                                //System.out.println("qwqwq");
+                                ps.println("Failed!");
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
 
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-				break;
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		Server test = new Server();
-		test.starServer(args);
-	}
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
 
 }
